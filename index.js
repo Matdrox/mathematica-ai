37a8a9bb643f50d51d5a4302b3b14d59e4bababa
Matei Cananau <mateicananau@gmail.com>
2023-02-01 18:20:53 +0100

    [Updated] Style

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bg-cyan': '#66AEAF'
+        'main': '#FFBE98'
       }
     },
   },
